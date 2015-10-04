6ab0537f156f56aeb7ef1b08d826274ce6fe9f51
Safia Abdalla <safia@safia.rocks>
2015-10-03 19:36:47 -0500

    Added initial API documentation

--- apps/front.py
+++ apps/front.py
@@ -8,5 +8,9 @@ app = Flask(__name__, static_folder = '../static', template_folder="../templates
 def index():
     return render_template("index.html")
 
+@app.route("/docs/api")
+def api_docs():
+    return render_template("api_docs.html")
+
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
